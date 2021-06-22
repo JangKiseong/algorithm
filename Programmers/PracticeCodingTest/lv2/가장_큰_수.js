9a75994415657385c7213df868b6614a7dbc372c
aurora9120 <aurora9120@naver.com>
2021-06-22 23:50:01 +0900

    Programmers lv2 해결 문제 등록
    가장 큰 수

--- Programmers/PracticeCodingTest/lv2/가장_큰_수.js
+++ Programmers/PracticeCodingTest/lv2/가장_큰_수.js
@@ -2,29 +2,12 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42746
 
 function solution(numbers) {
-
-    numbers.sort((pre, post) => pre-post);
-
-    return numbers.sort((pre, post) => {
-        if(pre < 10 && post < 10){
-            return post - pre;
-        } else if(pre >= 10 && post >= 10){
-
-            const preNumDigit = pre.toString().length-1;
-            const postNumDigit = post.toString().length-1;
-            return post/10**postNumDigit - pre/10**preNumDigit;
-
-        } else if(pre >= 10 && post < 10){
-
-            const preNumDigit = pre.toString().length-1;
-            return post - parseInt(pre/10**preNumDigit);
-
-        } else if(post >= 10 && pre < 10) {
-
-            const postNumDigit = post.toString().length-1;
-            return parseInt(post/10**postNumDigit) - pre;
-        }
+    const answer = numbers.sort((pre, post) => {
+        const preNum = pre + "" + post;
+        const postNum = post + "" + pre;
+        return postNum*1 - preNum*1;
     }).join("");
+    
+    if(answer*1 === 0) return "0";
+    else return answer;
 }
-
-console.log(solution([3, 30, 34, 5, 9]))
